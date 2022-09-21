88e9b16389b51fbb1f35637c06795c0f3fd9f48e
Maria <maria@yuyiss-mbp.lan>
2022-09-21 12:42:09 -0400

    Make tile grid with tile components

--- src/tile-board/Tile.jsx
+++ src/tile-board/Tile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Tile() {
   return (
-    <button type="button" class="btn btn-light btn-outline-dark">
+    <button type='button' className='btn btn-light btn-outline-dark'>
       Tile
     </button>
   )
--- src/tile-board/Tileboard.jsx
+++ src/tile-board/Tileboard.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import Tile from './Tile';
 
 function TileBoard() {
+
+  let allTiles = [];
+  for (let i=0; i<5; i++) {
+    let row = [];
+
+    for (let j=0; j<5; j++) {
+      row.push(<Tile key={`${i}, ${j}`} />);
+    }
+    allTiles.push(row);
+  }
+  
   return (
-    <div className='d-flex justify-content-center'>
-        Tile Board
-        <Tile />
+    <div className='container text-center mt-5'>
+      {
+        allTiles.map((row, key) => {
+          return <div key={key}>{row}</div>;
+        })
+      }
     </div>
   )
 }
