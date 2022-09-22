57de12a51df3ff08ddcb076ebda8c7814422bead
Maria Ordonez <mjordonez2001@gmail.com>
2022-09-22 18:07:02 -0400

    Add toggle for adjacent tiles

--- src/tile-board/Tileboard.jsx
+++ src/tile-board/Tileboard.jsx
@@ -6,12 +6,7 @@ function TileBoard() {
   const [arrayGrid, setArrayGrid] = useState(() => createArrayGrid(size));
 
   const handleToggle = ({ target }) => {
-    const { id } = target;
-
-    const tempArray = [...arrayGrid];
-    tempArray[id[0]][id[1]] = !tempArray[id[0]][id[1]];
-
-    setArrayGrid([...tempArray]);
+    setArrayGrid(toggle(arrayGrid, target.id, size));
   };
 
   return (
@@ -51,6 +46,36 @@ function createArrayGrid(size) {
   }
 
   return allTiles;
+}
+
+/**
+ * Toggles adjacent tiles
+ */
+function toggle(arrayGrid, id, size) {
+  const tempArray = [...arrayGrid];
+  const i = Number(id[0]);
+  const j = Number(id[1]);
+
+  tempArray[i][j] = !tempArray[i][j];
+
+  // top tile
+  if (i > 0) {
+    tempArray[i - 1][j] = !tempArray[i - 1][j];
+  }
+  // bottom tile
+  if (i < size - 1) {
+    tempArray[i + 1][j] = !tempArray[i + 1][j];
+  }
+  // right tile
+  if (j < size - 1) {
+    tempArray[i][j + 1] = !tempArray[i][j + 1];
+  }
+  // left tile
+  if (j < size - 1) {
+    tempArray[i][j - 1] = !tempArray[i][j - 1];
+  }
+
+  return tempArray;
 }
 
 export default TileBoard;
