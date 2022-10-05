911116dc7c51b9bc507c07a6b89765bce03e7b31
Maria Ordonez <mjordonez2001@gmail.com>
2022-10-05 16:02:33 -0400

    Change move counter color

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ function App() {
   const [arrayGrid, setArrayGrid] = useState(() => randomArrayGrid(size));
   const [moves, setMoves] = useState(0);
   const [winAlert, setWinAlert] = useState(false);
+  const [rgb, setRgb] = useState([0, 185, 70]);
 
   useEffect(() => {
     setArrayGrid(randomArrayGrid(size));
     setMoves(0);
     setWinAlert(false);
+    setRgb([0, 185, 70]);
   }, [size]);
 
   useEffect(() => {
@@ -25,6 +27,7 @@ function App() {
     setArrayGrid(randomArrayGrid(size));
     setMoves(0);
     setWinAlert(false);
+    setRgb([0, 185, 70]);
   };
 
   const onSizeChange = ({ target }) => {
@@ -34,6 +37,14 @@ function App() {
   const onToggle = (y, x) => {
     setArrayGrid(toggle(arrayGrid, y, x, size));
     setMoves((currentMoves) => currentMoves + 1);
+
+    if (rgb[1] > 5) {
+      const tempRgb = [...rgb];
+      tempRgb[0] += 3;
+      tempRgb[1] -= 3;
+      tempRgb[2] -= 1;
+      setRgb([...tempRgb]);
+    }
   };
 
   return (
@@ -61,7 +72,15 @@ function App() {
         <></>
       )}
 
-      <div>Moves: {moves}</div>
+      <div>
+        Moves:&nbsp;
+        <span
+          className="movesCounter"
+          style={{ color: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})` }}
+        >
+          {moves}
+        </span>
+      </div>
       <TileBoard arrayGrid={arrayGrid} onToggle={onToggle} />
       <button className="btn btn-primary mt-3" onClick={onRestart}>
         New game
